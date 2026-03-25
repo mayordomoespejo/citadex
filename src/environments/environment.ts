866be90fb40f766513c30288949cf300d08fca33
Miguel <miguelmayordomoespejo@gmail.com>
2026-03-25 12:48:42 +0100

    fix(auth): fill Firebase config for citadex project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   apiBaseUrl: 'https://rickandmortyapi.com/api',
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: 'AIzaSyDJgj4lwLOXUwLw1TM2apkKrDrqBhYpu4w',
+    authDomain: 'citadex-eeea0.firebaseapp.com',
+    projectId: 'citadex-eeea0',
+    storageBucket: 'citadex-eeea0.firebasestorage.app',
+    messagingSenderId: '949905048711',
+    appId: '1:949905048711:web:419d7c37c7d73b35302422',
   },
   supabaseUrl: '',
 };
